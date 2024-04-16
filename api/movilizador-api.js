88916a50727f6a40be7aa94606cce6ee98de3e28
Josué Castillo Morozumi <josuemorozumi@outlook.com>
2024-04-16 14:59:05 -0600

    Movilizadores

--- api/movilizador-api.js
+++ api/movilizador-api.js
@@ -1,36 +1,37 @@
-document.addEventListener('DOMContentLoaded', fetchData);
+document.addEventListener("DOMContentLoaded", fetchData);
 
 function fetchData() {
-    fetch('https://hcpboca.ddns.net:3050/api/getMovilizadores/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
+  fetch("https://hcpboca.ddns.net:3050/api/getMovilizadores/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
+    .then((posts) => {
+    console.log(posts)
+
+      displayData(posts);
     })
-    .then(posts => {
-        displayData(posts);
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
+    .catch((error) => {
+      console.error("Error fetching data:", error);
     });
 }
 
 function displayData(posts) {
-    const tableBody = document.querySelector('#contenido-tabla');
+  const tableBody = document.querySelector("#contenido-tabla");
 
-    // Limpiar cualquier fila existente en la tabla
-    tableBody.innerHTML = '';
+  // Limpiar cualquier fila existente en la tabla
+  tableBody.innerHTML = "";
 
-    // Iterar sobre los posts y agregarlos a la tabla
-    posts.forEach(post => {
-console.log(post)
-        const row = `
+  // Iterar sobre los posts y agregarlos a la tabla
+  posts.forEach((post) => {
+    const row = `
             
         
         <tr>
@@ -40,27 +41,42 @@ console.log(post)
                 </div>
             </td>
             <td>
-                <a href="#" class="text-gray-600 text-hover-primary mb-1">${post.username}</a>
+                <a href="#" class="text-gray-600 asd text-hover-primary mb-1">${
+                  post.nombre + " " + post.paterno + " " + post.materno
+                }</a>
             </td>
             <td>
-                <a href="#" class="text-gray-600 text-hover-primary mb-1">123</a>
+                <a href="#" class="text-gray-600 text-hover-primary mb-1"> ${
+                  post.calle +
+                  " " +
+                  post.direccion_ext +
+                  " " +
+                  post.direccion_int +
+                  ", " +
+                  post.colonia +
+                  ", " +
+                  post.c_postal
+                }</a>
             </td>
             <td>
-                <a href="#" class="text-gray-600 text-hover-primary mb-1">2291529343</a>
+                <a href="#" class="text-gray-600 text-hover-primary mb-1">${
+                  post.telefono
+                }</a>
             </td>
             <td>
-                <p class="text-gray-600 mb-1">123</p>
+                <p class="text-gray-600 mb-1">${post.seccion["numero"]}</p>
             </td>
             <td>
-                <p class="text-gray-600 mb-1">Boca del Río</p>
+                <p class="text-gray-600 mb-1">${post.municipio["nombre"]}</p>
             </td>
             <td>
-                <p class="text-gray-600 mb-1">Juan</p>
+                <a href="overview-lider.html?=${post.lider[0]['_id']}" class="text-gray-600 mb-1">${post.lider[0]['nombre']+" "+ post.lider[0]['paterno']+" "+post.lider[0]['materno']}</a>
             </td>
+
         </tr>
     <!--end::Table body-->
             
         `;
-        tableBody.innerHTML += row;
-    });
+    tableBody.innerHTML += row;
+  });
 }
