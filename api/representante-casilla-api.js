c1418bd23abfc26f0fb3841042fc54771764b760
FherDZ91 <fher91.fd@gmail.com>
2024-04-23 17:39:05 -0600

    Cambios para el buscador en las tablas, falta el boton de filtrado

--- api/representante-casilla-api.js
+++ api/representante-casilla-api.js
@@ -1,4 +1,8 @@
-document.addEventListener('DOMContentLoaded', fetchData);
+document.addEventListener('DOMContentLoaded', fetchData, KTCustomersList);
+
+// KTUtil.onDOMContentLoaded(function () {
+//     KTCustomersList.init();
+// });
 
 function fetchData() {
     fetch('https://hcpboca.ddns.net:3050/api/getRcs/', {
@@ -87,3 +91,106 @@ function displayData(posts) {
         tableBody.innerHTML += row;
     });
 }
+
+function KTCustomersList() {
+    var datatable;
+    var table;
+
+    // Private functions
+    var initCustomerList = function () {
+        // Set date data order
+        const tableRows = table.querySelectorAll('tbody tr');
+
+        // tableRows.forEach(row => {
+        //     const dateRow = row.querySelectorAll('td');
+        //     const realDate = moment(dateRow[5].innerHTML, "DD MMM YYYY, LT").format(); // select date from 5th column in table
+        //     dateRow[5].setAttribute('data-order', realDate);
+        // });
+
+        // Init datatable --- more info on datatables: https://datatables.net/manual/
+        datatable = $(table).DataTable({
+            "info": false,
+            'order': [],
+            'columnDefs': [
+                { orderable: false, targets: 0 }, // Disable ordering on column 0 (checkbox)
+                { orderable: false, targets: 6 }, // Disable ordering on column 6 (actions)
+            ]
+        });
+
+        // Re-init functions on every table re-draw -- more info: https://datatables.net/reference/event/draw
+        datatable.on('draw', function () {
+            // initToggleToolbar();
+            // handleDeleteRows();
+            // toggleToolbars();
+            //KTMenu.init(); // reinit KTMenu instances 
+        });
+    }
+
+    var handleSearchDatatable = () => {
+        const filterSearch = document.querySelector('[data-kt-customer-table-filter="search"]');
+        filterSearch.addEventListener('keyup', function (e) {
+            datatable.search(e.target.value).draw();
+        });
+    }
+
+    // Public methods
+    initCustomerList();
+    handleSearchDatatable();
+}
+
+// var KTCustomersList = function () {
+//     var datatable;
+//     var table;
+
+//     // Private functions
+//     var initCustomerList = function () {
+//         // Set date data order
+//         const tableRows = table.querySelectorAll('tbody tr');
+
+//         // tableRows.forEach(row => {
+//         //     const dateRow = row.querySelectorAll('td');
+//         //     const realDate = moment(dateRow[5].innerHTML, "DD MMM YYYY, LT").format(); // select date from 5th column in table
+//         //     dateRow[5].setAttribute('data-order', realDate);
+//         // });
+
+//         // Init datatable --- more info on datatables: https://datatables.net/manual/
+//         datatable = $(table).DataTable({
+//             "info": false,
+//             'order': [],
+//             'columnDefs': [
+//                 { orderable: false, targets: 0 }, // Disable ordering on column 0 (checkbox)
+//                 { orderable: false, targets: 6 }, // Disable ordering on column 6 (actions)
+//             ]
+//         });
+
+//         // Re-init functions on every table re-draw -- more info: https://datatables.net/reference/event/draw
+//         datatable.on('draw', function () {
+//             // initToggleToolbar();
+//             // handleDeleteRows();
+//             // toggleToolbars();
+//             //KTMenu.init(); // reinit KTMenu instances 
+//         });
+//     }
+
+//     var handleSearchDatatable = () => {
+//         const filterSearch = document.querySelector('[data-kt-customer-table-filter="search"]');
+//         filterSearch.addEventListener('keyup', function (e) {
+//             datatable.search(e.target.value).draw();
+//         });
+//     }
+
+//     // Public methods
+//     return {
+//         init: function () {
+//             table = document.querySelector('#kt_customers_table');
+            
+//             if (!table) {
+//                 return;
+//             }
+
+//             initCustomerList();
+//             handleSearchDatatable();
+
+//         }
+//     }
+// }();
