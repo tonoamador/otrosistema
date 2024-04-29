204191d420255dd345e3469d7f015125708d2856
Josué Castillo Morozumi <josuemorozumi@outlook.com>
2024-04-29 10:32:37 -0600

    login

--- api/overview-rc-api.js
+++ api/overview-rc-api.js
@@ -1,58 +1,60 @@
-const toastElement = document.getElementById("kt_docs_toast_toggle")
-const toast = bootstrap.Toast.getOrCreateInstance(toastElement)
+const toastElement = document.getElementById("kt_docs_toast_toggle");
+const toast = bootstrap.Toast.getOrCreateInstance(toastElement);
 var state = false;
-
-if(window.location.hash){
-    const params = new URLSearchParams(window.location.search);
-    const idRc = params.get("id");
+const token = JSON.parse(localStorage.getItem("token"));
+document.querySelector("#nombre").innerHTML =
+  token.paterno + " " + token.materno + " " + token.nombre;
+document.querySelector("#seccion").innerHTML = token.casilla.seccion.numero;
+document.querySelector("#municipio").innerHTML =
+  token.casilla.seccion.municipio.nombre;
+document.querySelector("#casilla").innerHTML = token.casilla.nombre;
+console.log(token);
+if (window.location.hash) {
+  const params = new URLSearchParams(window.location.search);
+  const idRc = params.get("id");
 }
 
-
 function buttonState(posts) {
-    // console.log(posts.data['open'])
-    return posts.data['open']
+  return posts.data["open"];
 }
 
 function OpenBox() {
-    
-    const e = document.getElementById("kt_user_follow_button");
-    const id = e.getAttribute("data-id-casilla");
-    $.ajax({
-        url: "https://hcpboca.ddns.net:3050/api/openCasilla/"+id,
-        dataType: "JSON",
-        method: 'POST',
-        async: false,
-        success: function (i) {
-            state = i.data['open']
-        }
-    })
-    return state
+  const e = document.getElementById("kt_user_follow_button");
+  const id = token.casilla._id;
+  $.ajax({
+    url: "https://hcpboca.ddns.net:3050/api/openCasilla/" + id,
+    dataType: "JSON",
+    method: "POST",
+    async: false,
+    success: function (i) {
+      state = i.data["open"];
+    },
+  });
+  return state;
 }
 
 function loadingPage() {
-    const loadingEl = document.createElement("div")
-    document.body.prepend(loadingEl)
-    loadingEl.classList.add("page-loader");
-    loadingEl.classList.add("flex-column");
-    loadingEl.classList.add("bg-dark");
-    loadingEl.classList.add("bg-opacity-25");
-    loadingEl.innerHTML = `
+  const loadingEl = document.createElement("div");
+  document.body.prepend(loadingEl);
+  loadingEl.classList.add("page-loader");
+  loadingEl.classList.add("flex-column");
+  loadingEl.classList.add("bg-dark");
+  loadingEl.classList.add("bg-opacity-25");
+  loadingEl.innerHTML = `
         <span class="spinner-border text-primary" role="status"></span>
         <span class="text-gray-800 fs-6 fw-semibold mt-5">Cargando...</span>
     `;
 
-    // Show page loading
-    KTApp.showPageLoading();
+  // Show page loading
+  KTApp.showPageLoading();
 
-    // Hide after 3 seconds
-    setTimeout(function() {
-        KTApp.hidePageLoading();
-        loadingEl.remove();
-        if(window.location.hash){
-
-        }else{
-            window.location.replace("ciudadanos-rc.html")
-        }
-        
-    }, 3000);
+  // Hide after 3 seconds
+  setTimeout(function () {
+    KTApp.hidePageLoading();
+    loadingEl.remove();
+    if (window.location.hash) {
+    } else {
+      window.location.replace("ciudadanos-rc.html");
+    }
+  }, 3000);
 }
---
+++ login/login.js
@@ -0,0 +1,101 @@
+"use strict";
+
+var KTSigninGeneral = (function () {
+  var t, e, r;
+  return {
+    init: function () {
+      (t = document.querySelector("#kt_sign_in_form")),
+        (e = document.querySelector("#kt_sign_in_submit")),
+        (r = FormValidation.formValidation(t, {
+          fields: {
+            username: {
+              validators: {
+                notEmpty: { message: "Se requiere usuario" },
+              },
+            },
+            password: {
+              validators: { notEmpty: { message: "Se requiere contraseña" } },
+            },
+          },
+          plugins: {
+            trigger: new FormValidation.plugins.Trigger(),
+            bootstrap: new FormValidation.plugins.Bootstrap5({
+              rowSelector: ".fv-row",
+              eleInvalidClass: "",
+              eleValidClass: "",
+            }),
+          },
+        }));
+
+      e.addEventListener("click", function (i) {
+        i.preventDefault(),
+          r.validate().then(function (r) {
+            if ("Valid" == r) {
+              e.setAttribute("data-kt-indicator", "on"),
+                (e.disabled = !0),
+                axios
+                  .post("https://hcpboca.ddns.net:3050/api/login/", {
+                    username: t.querySelector('[name="username"]').value,
+                    password: t.querySelector('[name="password"]').value,
+                  })
+                  .then(function (response) {
+                    const base64Url = response.data.token.split(".")[1];
+                    const base64 = base64Url
+                      .replace(/-/g, "+")
+                      .replace(/_/g, "/");
+                    const jsonPayload = decodeURIComponent(
+                      atob(base64)
+                        .split("")
+                        .map(function (c) {
+                          return (
+                            "%" +
+                            ("00" + c.charCodeAt(0).toString(16)).slice(-2)
+                          );
+                        })
+                        .join("")
+                    );
+
+                    const token = JSON.parse(jsonPayload);
+                    console.log(token);
+                    localStorage.setItem("token", JSON.stringify(token));
+                    Swal.fire({
+                      text: "You have successfully logged in!",
+                      icon: "success",
+                      buttonsStyling: !1,
+                      confirmButtonText: "Ok, got it!",
+                      customClass: { confirmButton: "btn btn-primary" },
+                    }).then(function (e) {
+                      if (e.isConfirmed) {
+                        var redirectUrl = t.getAttribute(
+                          "data-kt-redirect-url"
+                        );
+                        var additionalParams = "id=" + token._id;
+
+                        if (redirectUrl) {
+                          location.href = redirectUrl + additionalParams;
+                        }
+                      }
+                    });
+                  })
+                  .catch(function (error) {
+                    {
+                      Swal.fire({
+                        text: "Los datos ingresados son incorrectos, por favor intente de nuevo.",
+                        icon: "error",
+                        buttonsStyling: !1,
+                        confirmButtonText: "Ok, got it!",
+                        customClass: { confirmButton: "btn btn-primary" },
+                      });
+                    }
+                  })
+                  .finally(function () {
+                    e.removeAttribute("data-kt-indicator"), (e.disabled = !1);
+                  });
+            }
+          });
+      });
+    },
+  };
+})();
+
+KTSigninGeneral.init();
